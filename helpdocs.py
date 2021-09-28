6320e9454f9611a1db3ecef3424eb4e31b0dd65f
Raglik <Kilgarr1928@gmail.com>
2021-09-27 20:45:49 -0600

    Deleted ppv1 and ppv1_unstable, they are no longer commands

--- helpdocs.py
+++ helpdocs.py
@@ -793,26 +793,6 @@ def help(arg=None):
 -u: specify a user (For a space in the username, use "+")
 ```""",inline=False)
 
-#    elif arg == "ppv1":
-#        embed.title = "!ppv1"
-#        embed.description = "**Description:** The first performance point system leaderboards; however, the algorithm is not public knowledge \
-#                                              so it is just a rough guess of how it worked."
-#        embed.add_field(name="Global parameters", value="""```ahk
-#-l: specify how many results to output. Beware the 2000 character limit
-#-p: specify the resulting page to output
-#-u: specify a user (For a space in the username, use "+")
-#```""",inline=False)
-
-#    elif arg == "ppv1_unstable":
-#        embed.title = "!ppv1_unstable"
-#        embed.description = "**Description:** The first performance point system leaderboards; however, the algorithm is not public knowledge \
-#                                              so it is just a rough guess of how it worked and pp values are not inflated."
-#        embed.add_field(name="Global parameters", value="""```ahk
-#-l: specify how many results to output. Beware the 2000 character limit
-#-p: specify the resulting page to output
-#-u: specify a user (For a space in the username, use "+")
-#```""",inline=False)
-
     elif arg == "totalpp":
         embed.title = "!totalpp"
         embed.description = "**Description:** PP Leaderboard if PP wasn't weighted."
