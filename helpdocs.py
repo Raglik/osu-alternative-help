16574341d811392daa02e3b57f4e0a9a19202973
Raglik <Kilgarr1928@gmail.com>
2021-09-30 01:30:37 -0600

    fixed uploadscores

--- helpdocs.py
+++ helpdocs.py
@@ -754,11 +754,7 @@ def help(arg=None):
         embed.description = "**Description:** Upload a .csv file from the score fetcher (>scorefetcher) which will import the scores into the database. \
                                               Attach the .csv with the command, if it works should display a sending message. To check the queue time, use !uploadqueue."
         embed.add_field(name="Command parameters", value="""```ahk
--b: Which beatmap id to check
--u: Which user id to check
-```""",inline=False)
-        embed.add_field(name="Example", value="""```ahk
-!scorequeue -u Kilgar -b 75
+NONE
 ```""",inline=False)
 
 #PP commands
