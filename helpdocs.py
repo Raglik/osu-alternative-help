20e9cabdb74ff136dfcce8db54062fc1864f65be
Raglik <Kilgarr1928@gmail.com>
2021-11-02 14:27:37 -0600

    added help for missing commands

--- helpdocs.py
+++ helpdocs.py
@@ -11,7 +11,6 @@ def help(arg=None):
 `clears`
 `gold_s`
 `gold_ss`
-`hitsperplay`
 `playcount`
 `playtime`
 `silver_s`
@@ -97,6 +96,7 @@ def help(arg=None):
         embed.add_field(name = "Other Stats:", value = """
 `fc_count`
 `followers`
+`hitsperplay`
 `most_medals`
 `oldestnumberone`
 `replayswatched`
@@ -147,7 +147,6 @@ def help(arg=None):
 -title: queue a subset of maps with a given title
 -mapper: queue a subset of maps with a given mapper name
 -artist: queue a subset of maps with a given artist name
--diff: queue a subset of maps with a given difficulty name
 ```""",inline=False)
 
 #Global Stats commands
@@ -197,18 +196,6 @@ def help(arg=None):
         embed.add_field(name="Optional parameters", value="""```ahk
 -country: specify a country using the ISO 2 letter code
 -filter: specify a year or a difficulty range if applicable
-```""",inline=False)
-        embed.add_field(name="Global parameters", value="""```ahk
--l: specify how many results to output. Beware the 4000 character limit
--p: specify the resulting page to output
--u: specify a user (For a space in the username, use "+")
-```""",inline=False)
-
-    elif arg == "hitsperplay":
-        embed.title = "!hitsperplay"
-        embed.description = "Hitsperplay leaderboard"
-        embed.add_field(name="Optional parameters", value="""```ahk
--country: specify a country using the ISO 2 letter code
 ```""",inline=False)
         embed.add_field(name="Global parameters", value="""```ahk
 -l: specify how many results to output. Beware the 4000 character limit
@@ -650,6 +637,38 @@ def help(arg=None):
         status in conjuction with -mods 1
         • 1,1.06,1.12,1.19,1.25,1.33,1.41
 -mods 1: Returns the amount of beatmaps based on the mod score (use in conjunction -status)
+```""",inline=False)
+        embed.add_field(name="Optional parameters", value="""```ahk
+-letter: X XH SH S A B C D
+-is_ss,-is_fc,-is_ht,-is_dt,-is_hr and etc. : true/false
+-replay: true/false
+-order: score, length, approved_date, accuracy, ar, od etc.
+-direction: desc, asc
+-min: minimal star rating of maps to include (inclusive)
+-max: maximal star rating of maps to include (exclusive)
+-start: earliest rank date of maps to include
+-end: latest rank date of maps to include
+-year: specify a year
+-unplayed: true/false
+```""",inline=False)
+        embed.add_field(name="Beatmap parameters", value="""```ahk
+-ar-min, -od-max, -cs-min, -length-max, etc: map parameters
+-tags: queue a subset of maps with given tags
+-title: queue a subset of maps with a given title
+-mapper: queue a subset of maps with a given mapper name
+-artist: queue a subset of maps with a given artist name
+```""",inline=False)
+        embed.add_field(name="Global parameters", value="""```ahk
+-l: specify how many results to output. Beware the 4000 character limit
+-p: specify the resulting page to output
+-u: specify a user (For a space in the username, use "+")
+```""",inline=False)
+
+    elif arg == "missingscore":
+        embed.title = "!missingscore"
+        embed.description = "**Description:**Returns an ordered list of plays based on how much score you're msising compared to the #1 play on the map."
+        embed.add_field(name="Command parameters", value="""```ahk
+-o: nomodscore
 ```""",inline=False)
         embed.add_field(name="Optional parameters", value="""```ahk
 -letter: X XH SH S A B C D
@@ -1266,6 +1285,44 @@ def help(arg=None):
 -u: specify a user (For a space in the username, use "+")
 ```""",inline=False)
 
+    elif arg == "followers":
+        embed.title = "!followers"
+        embed.description = "Followers count leaderboard"
+        embed.add_field(name="Optional parameters", value="""```ahk
+-country: specify a country using the ISO 2 letter code
+-rankedscore: min. score for a user to be on the board
+```""",inline=False)
+        embed.add_field(name="Global parameters", value="""```ahk
+-l: specify how many results to output. Beware the 4000 character limit
+-p: specify the resulting page to output
+-u: specify a user (For a space in the username, use "+")
+```""",inline=False)
+
+    elif arg == "hitsperplay":
+        embed.title = "!hitsperplay"
+        embed.description = "Hitsperplay leaderboard"
+        embed.add_field(name="Optional parameters", value="""```ahk
+-country: specify a country using the ISO 2 letter code
+```""",inline=False)
+        embed.add_field(name="Global parameters", value="""```ahk
+-l: specify how many results to output. Beware the 4000 character limit
+-p: specify the resulting page to output
+-u: specify a user (For a space in the username, use "+")
+```""",inline=False)
+
+    elif arg == "most_medals":
+        embed.title = "!most_medals"
+        embed.description = "Medal count leaderboard"
+        embed.add_field(name="Optional parameters", value="""```ahk
+-country: specify a country using the ISO 2 letter code
+-rankedscore: min. score for a user to be on the board
+```""",inline=False)
+        embed.add_field(name="Global parameters", value="""```ahk
+-l: specify how many results to output. Beware the 4000 character limit
+-p: specify the resulting page to output
+-u: specify a user (For a space in the username, use "+")
+```""",inline=False)
+
     elif arg == "oldestnumberone":
         embed.title = "!oldestnumberone"
         embed.description = "**Description:** oldest number ones <user ID> | <beatmap ID>"
@@ -1291,19 +1348,6 @@ def help(arg=None):
         embed.description = "**Description:** Top 50 global leaderboards."
         embed.add_field(name="Required parameters", value="""```ahk
 -filter: Specify a year (use full for every year)
-```""",inline=False)
-        embed.add_field(name="Global parameters", value="""```ahk
--l: specify how many results to output. Beware the 4000 character limit
--p: specify the resulting page to output
--u: specify a user (For a space in the username, use "+")
-```""",inline=False)
-
-    elif arg == "most_medals":
-        embed.title = "!most_medals"
-        embed.description = "Medal count leaderboard"
-        embed.add_field(name="Optional parameters", value="""```ahk
--country: specify a country using the ISO 2 letter code
--rankedscore: min. score for a user to be on the board
 ```""",inline=False)
         embed.add_field(name="Global parameters", value="""```ahk
 -l: specify how many results to output. Beware the 4000 character limit
@@ -1406,7 +1450,10 @@ def help(arg=None):
         embed.title = "!register"
         embed.description = "**Description:** Registers a player for more frequent updates"
         embed.add_field(name="Required parameter", value="""```ahk
--u: Username or user id if username does not work
+Username or user id if username does not work
+```""",inline=False)
+        embed.add_field(name="Example", value="""```ahk
+!register 8967040 or !register Kilgar
 ```""",inline=False)
 
 #Secret Commands
